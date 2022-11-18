fd8396d1ed3e00867d5e0ab013da03b8e9db490f
phuhung9292 <phuhungx1810@gmail.com>
2022-11-18 10:58:25 +0700

    add title component

---
+++ src/components/common/title/Tiltle.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Tiltle(props) {
+  return (
+    <div style={{ color: "#e30613" }}>
+      <h3>{props.title}</h3>
+    </div>
+  );
+}
+
+export default Tiltle;
