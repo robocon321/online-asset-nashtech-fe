c35328088ffd5e081c4cecb96981cb36a99db2d3
robocon321 <robocon321n@gmail.com>
2022-11-19 10:26:35 +0700

    main resolve conflict

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import HomeProvider from "./contexts/providers/HomeProvider";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HomeProvider>
       <App />
-    </BrowserRouter>
+    </HomeProvider>
   </React.StrictMode>
 );
 
