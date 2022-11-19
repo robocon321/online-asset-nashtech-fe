384d03cc86ae6ca464e57f3d48fc9184ecb3daad
robocon321 <robocon321n@gmail.com>
2022-11-20 02:36:04 +0700

    T943 Refactor code validate v1

--- src/contexts/actions/CreateUserAction.js
+++ src/contexts/actions/CreateUserAction.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 export const ACTIONS = {
   SET_FIELD: 'SET_FIELD',
   SET_ENABLE_SUBMIT: 'SET_ENABLE_SUBMIT',
-  SET_ERROR: 'SET_ERROR',
   SET_FIELD_ERROR: 'SET_FIELD_ERROR',
-  SET_LOADING: 'SET_LOADING'
+  SET_LOADING: 'SET_LOADING',
+  REMOVE_FIELD_ERROR: 'REMOVE_FIELD_ERROR',
+  ADD_FIELD_ERROR: 'ADD_FIELD_ERROR'
 }
 
 export const setFieldAction = (name, value) => (dispatch) => {
@@ -20,13 +21,6 @@ export const setEnableSubmitAction = (enable) => (dispatch) => {
     type: ACTIONS.SET_ENABLE_SUBMIT,
     payload: enable
   });
-}
-
-export const setErrorAction = (error) => (dispatch) => {
-  dispatch({
-    type: ACTIONS.SET_ERROR,
-    payload: error
-  })
 }
 
 export const setLoadingAction = (isLoading) => dispatch => {
@@ -51,4 +45,18 @@ export const submitAction = (form, navigate) => async (dispatch) => {
  }, 1000);
 
  setLoadingAction(true)(dispatch);
+}
+
+export const addErrorFieldAction = (name, value) => dispatch => {
+  dispatch({
+    type: ACTIONS.ADD_FIELD_ERROR,
+    payload: {name, value}
+  })
+}
+
+export const removeErrorFieldAction = (name) => dispatch => {
+  dispatch({
+    type: ACTIONS.REMOVE_FIELD_ERROR,
+    payload: name
+  })
 }
